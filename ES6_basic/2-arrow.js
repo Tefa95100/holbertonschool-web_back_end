1dd2dc137a0ec135d7ce3cab9d72ebe0564e8a83
Tefa95100 <damien.julien.95100@gmail.com>
2025-03-26 20:57:20 +0100

    Replace self by this

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -3,8 +3,8 @@ export default function getNeighborhoodsList() {
   
 	const self = this;
 	this.addNeighborhood = (newNeighborhood) => {
-		self.sanFranciscoNeighborhoods.push(newNeighborhood);
-		return self.sanFranciscoNeighborhoods;
+		this.sanFranciscoNeighborhoods.push(newNeighborhood);
+		return this.sanFranciscoNeighborhoods;
 	  };
   }
 
