1015cdcae839b73118f7bb04a99c8205ceddf715
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-02 22:22:46 +0200

    Refactorisation of exercise 3 and add exercise 5

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,5 @@ export default function getStudentIdsSum(studentList) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  return studentList.map((studentList) => studentList.id).reduce(
-    (acc, current) => acc + current, 0,
-  );
+  return studentList.reduce((acc, studentList) => acc + studentList.id, 0);
 }
