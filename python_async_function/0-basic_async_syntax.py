a47467a9aa86b44c2a223fa72e00ee85cac46654
Tefa95100 <damien.julien.95100@gmail.com>
2025-04-23 21:41:02 +0200

    Add annotation for max delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous coroutine that waits for a random delay
     between 0 and max_delay seconds.
