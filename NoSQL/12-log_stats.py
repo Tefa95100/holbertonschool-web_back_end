beb09b2827709dc9d53b9374b67d2a86f8ba5f02
Tefa95100 <damien.julien.95100@gmail.com>
2025-05-03 22:19:54 +0200

    Correction for pycodestyle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,8 +8,8 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
+
     total_logs = collection.count_documents({})
-    
     print(f"{total_logs} logs")
 
     print("Methods:")
@@ -18,5 +18,6 @@ if __name__ == "__main__":
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print(f"{status_count} status check")
