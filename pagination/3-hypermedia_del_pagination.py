ebbdfc10f68e91a0da7ef4d66feafc5952d6b11f
Tefa95100 <damien.julien.95100@gmail.com>
2025-08-15 19:35:05 +0200

    Change assert index

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -60,7 +60,7 @@ class Server:
         """
         assert isinstance(index, int) and index >= 0
         indexed_dataset = self.indexed_dataset()
-        assert index < len(self.dataset())
+        assert index < max(indexed_dataset.keys()) + 1
 
         data = []
         current_index = index
