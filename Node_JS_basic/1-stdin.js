1656e4ac37c077055ba19829bd1419cd5eb73b02
Tefa95100 <damien.julien.95100@gmail.com>
2025-09-04 16:21:01 +0200

    Change first method display from console.log to process.stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf-8');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
   process.stdout.write(`Your name is: ${input}`);
