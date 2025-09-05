1d24c38fe27545f665e4ef8f6b8f1393c5ff3fc4
Tefa95100 <damien.julien.95100@gmail.com>
2025-09-05 14:19:05 +0200

    Change for eslint

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
-const http = require("node:http");
+const http = require('node:http');
 
-//Create the server
+// Create the server
 http.createServer((request, response) => {
-	//Reception of the request
-	let body = [];
-	request.on("data", chunk => {
-		body.push(chunk);
-	}).on("end", () => {
-		body = Buffer.concat(body).toString();
-		//response
-		response.end("Hello Holberton School!");
-	});
+  // Reception of the request
+  let body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    // response
+    response.end('Hello Holberton School!');
+  });
 })
-//Port to listen
-.listen(1245);
+// Port to listen
+  .listen(1245);
